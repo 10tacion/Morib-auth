e38b754982704db57b1d3bdb193f7519a77c2b6d
10tentacion <ddww1029wwdd@gmail.com>
2025-08-02 13:39:48 +0900

    add apple login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,11 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect } from "react";
+import { z } from "zod";
+
+const searchSchema = z.object({
+  type: z
+    .union([z.literal("electron"), z.literal("web")])
+    .default("web")
+    .catch("web"),
+});
 
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
+  validateSearch: searchSchema,
 });
 
 function RouteComponent() {
+  const search = Route.useSearch();
+
   // 페이지 진입 시 애니메이션 효과 적용
   useEffect(() => {
     // 페이지 요소들이 DOM에 추가된 후 애니메이션 클래스 추가
@@ -16,10 +27,23 @@ function RouteComponent() {
     return () => clearTimeout(timer);
   }, []);
 
-  const API_URL = `${import.meta.env.VITE_GOOGLE_URL}?client_type=electron`;
+  const GOOGLE_URL = `${import.meta.env.VITE_GOOGLE_URL}${search.type === "electron" ? "?client_type=electron" : ""}`;
+  const APPLE_URL = `${import.meta.env.VITE_APPLE_URL}${search.type === "electron" ? "?client_type=electron" : ""}`;
 
   const handleGoogleLogin = () => {
-    window.location.href = API_URL;
+    if (search.type == null) {
+      return;
+    }
+
+    window.location.href = GOOGLE_URL;
+  };
+
+  const handleAppleLogin = () => {
+    if (search.type == null) {
+      return;
+    }
+
+    window.location.href = APPLE_URL;
   };
 
   return (
@@ -49,13 +73,23 @@ function RouteComponent() {
 
           {/* 버튼 - 피그마 기준 비율 유지 */}
 
-          <button
-            type="button"
-            className="w-[39.5rem] cursor-pointer subhead-bold-20 fade-in-up"
-            onClick={handleGoogleLogin}
-          >
-            <img src="/google_login.svg" alt="구글 로그인 버튼" />
-          </button>
+          <div className="flex flex-col gap-[2rem]">
+            <button
+              type="button"
+              className="cursor-pointer subhead-bold-20 fade-in-up"
+              onClick={handleGoogleLogin}
+            >
+              <img src="/google_login.svg" alt="구글 로그인 버튼" />
+            </button>
+
+            <button
+              type="button"
+              className="cursor-pointer subhead-bold-20 fade-in-up"
+              onClick={handleAppleLogin}
+            >
+              <img src="/apple_login.svg" alt="구글 로그인 버튼" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
